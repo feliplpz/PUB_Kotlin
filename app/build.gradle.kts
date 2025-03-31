c95481d50e7710c55e96a0df8d6b36214016164b
Felipe Lopez <felipe_lopez@usp.br>
2025-03-31 01:28:40 -0300

    melhorias no servidor: - inicia refatoração das classes bluetooth - inicia implementação de uma ui amigável - implementa uma otimização no fluxo de dados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.projetoickotlinempty"
+    namespace = "br.usp.poli.pocketexperimentalphysics"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.projetoickotlinempty"
+        applicationId = "br.usp.poli.pocketexperimentalphysics"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ProjetoICKotlinEmpty"
+rootProject.name = "pocketexperimentalphysics"
 include(":app")
- 
